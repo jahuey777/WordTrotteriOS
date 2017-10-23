e39fb3af1dda63603cba756909c607a89b953fb6
jaime jahuey <jaimejahuey@jaimes-MacBook-Pro.local>
2017-10-23 12:14:09 -0400

    Finished Chapter 3.

--- WordTrotter/ViewController.swift
+++ WordTrotter/ViewController.swift
@@ -13,13 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //The frame of the UIView
+//        let firstName = CGRect(x:160, y:240, width: 100, height:150)
+//        let firstView = UIView(frame:firstName)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//
+//        let secondFrame = CGRect(x:20, y:30, width:50, height:50)
+//        let secondView = UIView(frame:secondFrame)
+//        secondView.backgroundColor = UIColor.green
+//        firstView.addSubview(secondView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
