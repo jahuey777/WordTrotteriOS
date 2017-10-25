d64055c416f0cf3adee80bfebb6df5424b80938e
jaime jahuey <jaimejahuey@jaimes-MacBook-Pro.local>
2017-10-25 17:29:09 -0400

    Finished Chapter 4

--- WordTrotter/ConversionViewController.swift
+++ WordTrotter/ConversionViewController.swift
@@ -8,22 +8,40 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController{
+class ConversionViewController: UIViewController, UITextFieldDelegate{
     
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        updateCelsiusLabel()
-    }
+    //constant number formatter
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
     
     var fahrenheitValue: Measurement<UnitTemperature>?{
         //called every time a new value for farenheitValue is set
         didSet{
             updateCelsiusLabel()
         }
+    }
+    
+    //computes the celsiusValue
+    var celsiusValue: Measurement<UnitTemperature>?{
+        if let fahrenheitValue = fahrenheitValue{
+            return fahrenheitValue.converted(to: .celsius)
+        }else{
+            return  nil
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
     }
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField){
@@ -48,23 +66,42 @@ class ConversionViewController: UIViewController{
         textField.resignFirstResponder()
     }
     
-    //computes the celsiusValue
-    var celsiusValue: Measurement<UnitTemperature>?{
-        if let fahrenheitValue = fahrenheitValue{
-            return fahrenheitValue.converted(to: .celsius)
-        }else{
-            return  nil
-        }
-    }
-    
     //Called in the didSet after the fahrentheit is set.
     func updateCelsiusLabel(){
         //This is when the celsius value gets calculated.
         if let celsiusValue = celsiusValue{
-            celsiusLabel.text = "\(celsiusValue.value)"
+//            celsiusLabel.text = "\(celsiusValue.value)"
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         }else {
             celsiusLabel.text = "???"
         }
     }
+    
+    //The textfield calls this method on its delegate, UITextFieldDelegate
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string:String)-> Bool{
+//        print("current text: \(textField.text)")
+//        print("replacement text: \(string)")
+
+        //reject the change if input already has one decimal in the text
+        let existinTextHasDecimalSeperator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        
+        //Wont allow alphabetic charecters. Could have happened if user pasted or typed with a keyboard 
+        let letters = NSCharacterSet.letters
+        let existingTextHasLetters = textField.text?.rangeOfCharacter(from: letters)
+        let replacementTextHasLetters = string.rangeOfCharacter(from: letters)
+        
+        if existinTextHasDecimalSeperator != nil, replacementTextHasDecimalSeperator != nil{
+            return false
+        }
+        else if existingTextHasLetters != nil || replacementTextHasLetters != nil{
+            return false
+        }
+
+        return true
+    }
+    
+
+    
     
 }
